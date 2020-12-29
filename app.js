ebc1d406e69119fed6ef8abf08ffccb771ebd30c
Fernando Lopes <fernando.lopes@hephaenergy.com.br>
2020-12-29 17:46:46 -0300

    root com o nome do desafio

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { db } from './db/index.js';
 import { app } from './routes/userRoutes.js';
 import dotenv from 'dotenv';
+// import swaggerJsdoc from 'swagger-jsdoc';
 
 dotenv.config();
 global.BR_OFFSET = 3*60*60*1000;
@@ -18,8 +19,29 @@ global.BR_OFFSET = 3*60*60*1000;
   }
 })();
 
-const port = process.env.PORT || 3000;
+// const swaggerDefinition = {
+//   info: {
+//     title: 'Node Swagger API',
+//     version: '1.0.0',
+//     description: 'Demonstrating how to describe a RESTful API with Swagger',
+//   },
+//   host: 'localhost:3000',
+//   basePath: '/',
+// };
+// const options = {
+//   // import swaggerDefinitions
+//   swaggerDefinition: swaggerDefinition,
+//   // path to the API docs
+//   apis: ['./routes/*.js'],
+// };
+// const swaggerSpec = swaggerJsdoc(options);
 
+// app.get('/', (req, res) => {
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(swaggerSpec);
+// });
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   try {
     console.log('API started');
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,9 @@ import middleware from './middleware.js';
 const app = express();
 app.use(express.json());
 
+app.get('/', async (req, res) => {
+  res.send('Desafio Pitang');
+});
 
 app.post('/signup', signup);
 
