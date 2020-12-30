84ae9af026d6f319b4e088b8da6a2ff249ae0797
Fernando Lopes <fernando.lopes@hephaenergy.com.br>
2020-12-30 10:26:07 -0300

    desafio completed

--- app.js
+++ app.js
@@ -16,34 +16,6 @@ db.mongoose.connect(db.url, {
   }
 });
 
-// const options = {
-//   definition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "LogRocket Express API with Swagger",
-//       version: "0.1.0",
-//       description:
-//         "This is a simple CRUD API application made with Express and documented with Swagger",
-//     },
-//     servers: [
-//       {
-//         url: "https://desafio-pitang-fl.herokuapp.com/",
-//       },
-//     ],
-//   },
-//   apis: ['./routes/userRoutes.js'],
-// };
-
-// import swaggerJsdoc from 'swagger-jsdoc';
-// import swaggerUi from 'swagger-ui-express';
-
-// const specs = swaggerJsdoc(options);
-// app.use(
-//   "/api-docs",
-//   swaggerUi.serve,
-//   swaggerUi.setup(specs)
-// );
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   try {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-
 app.get('/', (_, res) => {
   res.send('Desafio Pitang');
 });
