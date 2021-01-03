a97b0a40397a366706ec60eb8122588429a391e3
Anderson <48699535+andeer0601@users.noreply.github.com>
2021-01-02 21:49:17 -0300

    Heroku Adjust 2

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const app = require('./src/app.js')
+const express = require ('express')
+require('dotenv').config({
+    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+})
+
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./src/routes')
+
+const app = express();
+
+require('./src/database/database')
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(process.env.PORT || 4000 , function(){
   console.log(process.env.API_NAME + ' ON ' + ( process.env.PORT || 4000 ))
