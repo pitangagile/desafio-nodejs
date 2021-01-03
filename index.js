1747d8b7ef1f3ed955fd0d3dc46fec4f6040742e
Anderson <48699535+andeer0601@users.noreply.github.com>
2021-01-02 21:30:21 -0300

    Heroku port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/app')
 
-app.listen(process.env.API_PORT, function(){
-  console.log(process.env.API_NAME + ' ON ' + process.env.API_PORT)
+app.listen(process.env.PORT || 4000 , function(){
+  console.log(process.env.API_NAME + ' ON ' + ( process.env.PORT || 4000 ))
 });
