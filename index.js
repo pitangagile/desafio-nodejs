36e58a79bf1e1f2dd055dd6e37150df4245a6e88
Allan Ericson <allanericson@gmail.com>
2022-03-01 02:58:46 -0300

    Adding cors config + console config init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Server running" });
+});
+
+app.use(require("./src/app/controllers/AuthController"));
+
+app.listen(3000, () => {
+  console.log("App listening on port: 3000...");
+});
+
+module.exports = app;
