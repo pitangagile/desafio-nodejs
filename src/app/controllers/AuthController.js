6c2a4aeb3e0ad5c9c9988758f15889249e76b06d
Allan Ericson <allanericson@gmail.com>
2022-03-01 03:03:48 -0300

    Fix module name error

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const config = require("../../config/env.json");
